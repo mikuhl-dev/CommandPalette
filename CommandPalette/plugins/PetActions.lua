ef1e0725f9455bb6bd4c1a25c49c6d2149a062fe
michael <github@mikuhl.me>
2023-08-20 18:03:56 -0400

    fix: pet actions

--- CommandPalette/plugins/PetActions.lua
+++ CommandPalette/plugins/PetActions.lua
@@ -8,11 +8,11 @@ EventRegistry:RegisterCallback("CommandPalette.UpdateActions", function()
         local name = petActionInfo[1]
         local texture = petActionInfo[2]
         if name ~= nil then
-            local title = string.format(L["Call Pet: %s"], name)
+            local title = string.format(L["Use Pet Action: %s"], _G[name])
             if CommandPalette:MatchesSearch(title) then
                 CommandPalette:AddAction({
                     title = title,
-                    icon = texture,
+                    icon = _G[texture],
                     action = {
                         type = "pet",
                         action = i,
