fd8905f862c361cfe8e58a0d6cb58f33c7acc42f
michael <github@mikuhl.me>
2023-08-26 12:27:46 -0400

    refactor: cache emotes with hash list

--- CommandPalette/plugins/Emotes.lua
+++ CommandPalette/plugins/Emotes.lua
@@ -2,20 +2,30 @@ local _, addon = ...;
 
 local L = addon.L;
 
+local cached = false;
+local emotes = {};
+
 EventRegistry:RegisterCallback("CommandPalette.UpdateActions", function()
-    for i = 1, 672 do
-        local command = _G["EMOTE" .. i .. "_CMD1"];
-        if command ~= nil then
-            local title = string.format(L["Use Emote: %s"], command);
-            if CommandPalette:MatchesSearch(title) then
-                CommandPalette:AddAction({
-                    title = title,
-                    action = {
-                        type = "macro",
-                        macrotext = command,
-                    }
-                });
-            end;
+    if not cached then
+        ChatFrame_ImportAllListsToHash();
+
+        for command in pairs(hash_EmoteTokenList) do
+            emotes[string.lower(command)] = true;
+        end;
+
+        cached = true;
+    end;
+
+    for command in pairs(emotes) do
+        local title = string.format(L["Use Emote: %s"], command);
+        if CommandPalette:MatchesSearch(title) then
+            CommandPalette:AddAction({
+                title = title,
+                action = {
+                    type = "macro",
+                    macrotext = command,
+                }
+            });
         end;
     end;
 end);
