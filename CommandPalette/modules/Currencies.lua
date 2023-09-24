95ebd612b5c6b0e817ab26be1150de5f41e5d028
michael <github@mikuhl.me>
2023-09-23 20:03:55 -0400

    refactor: make currency logic safer

--- CommandPalette/modules/Currencies.lua
+++ CommandPalette/modules/Currencies.lua
@@ -18,14 +18,19 @@ local function EnumerateCurrencies()
     return function()
         while true do
             index = index + 1;
-            local currencyInfo = C_CurrencyInfo.GetCurrencyListInfo(index);
-            if currencyInfo == nil then
+            if index > C_CurrencyInfo.GetCurrencyListSize() then
+                -- Reset remaining headers.
+                C_CurrencyInfo.ExpandCurrencyList(headerIndex, headerExpanded);
                 return nil;
-            elseif currencyInfo.isHeader then
+            end;
+            local currencyInfo = C_CurrencyInfo.GetCurrencyListInfo(index);
+            if currencyInfo.isHeader then
+                -- Reset previous header.
                 C_CurrencyInfo.ExpandCurrencyList(headerIndex, headerExpanded);
                 if not headerExpanded then
                     index = index - numCurrencies;
                 end;
+                -- Expand current header.
                 headerIndex = index;
                 headerExpanded = currencyInfo.isHeaderExpanded;
                 numCurrencies = 0;
@@ -54,37 +59,35 @@ CommandPalette.RegisterModule(L["Currencies"], {
 
         actions = {};
         for index, currencyInfo in EnumerateCurrencies() do
-            if not currencyInfo.isHeader then
-                local name = currencyInfo.name;
-                local currencyLink = C_CurrencyInfo.GetCurrencyListLink(index);
-                local currencyID = C_CurrencyInfo.GetCurrencyIDFromLink(currencyLink);
-                table.insert(actions, {
-                    name = string.format(L["View Currency: %s"], name),
-                    icon = currencyInfo.iconFileID,
-                    tooltip = GenerateClosure(GameTooltip.SetCurrencyByID, GameTooltip, currencyID),
-                    pickup = GenerateClosure(C_CurrencyInfo.PickupCurrency, currencyID),
-                    action = {
-                        type = "currency",
-                        _currency = function()
-                            for index, currencyInfo in EnumerateCurrencies() do
-                                if currencyInfo.name == name then
-                                    LoadAddOn("Blizzard_TokenUI");
-                                    HideUIPanel(CharacterFrame);
-                                    ToggleCharacter("TokenFrame", true);
-                                    local scrollBox = TokenFrame.ScrollBox;
-                                    local elementData = scrollBox:FindElementData(index);
-                                    if elementData == nil then return; end;
-                                    scrollBox:ScrollToElementData(elementData);
-                                    local button = scrollBox:FindFrame(elementData);
-                                    if button == nil then return; end;
-                                    button:Click();
-                                    break;
-                                end;
+            local name = currencyInfo.name;
+            local currencyLink = C_CurrencyInfo.GetCurrencyListLink(index);
+            local currencyID = C_CurrencyInfo.GetCurrencyIDFromLink(currencyLink);
+            table.insert(actions, {
+                name = string.format(L["View Currency: %s"], name),
+                icon = currencyInfo.iconFileID,
+                tooltip = GenerateClosure(GameTooltip.SetCurrencyByID, GameTooltip, currencyID),
+                pickup = GenerateClosure(C_CurrencyInfo.PickupCurrency, currencyID),
+                action = {
+                    type = "currency",
+                    _currency = function()
+                        for index, currencyInfo in EnumerateCurrencies() do
+                            if currencyInfo.name == name then
+                                LoadAddOn("Blizzard_TokenUI");
+                                HideUIPanel(CharacterFrame);
+                                ToggleCharacter("TokenFrame", true);
+                                local scrollBox = TokenFrame.ScrollBox;
+                                local elementData = scrollBox:FindElementData(index);
+                                if elementData == nil then return; end;
+                                scrollBox:ScrollToElementData(elementData);
+                                local button = scrollBox:FindFrame(elementData);
+                                if button == nil then return; end;
+                                button:Click();
+                                break;
                             end;
-                        end,
-                    }
-                });
-            end;
+                        end;
+                    end,
+                }
+            });
         end;
 
         return actions;
